a39e4cb4eb33c8a18135fa40186fcef32136e383
Weverton-profile <wevertonalves.profile@gmail.com>
2022-07-02 15:10:05 -0300

    comparando o valor das cartas
    comparando o valor das cartas em caso de valores iguais as cartas permanecem viradas.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
 }
 
 cards.forEach((card) => {
