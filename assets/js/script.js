312e9908fd1a3d4a64070ecc0359c3095e728667
Weverton-profile <wevertonalves.profile@gmail.com>
2022-07-02 15:33:04 -0300

    corrigindo bugs
    bug de clicar 2 vezes na mesma carta já virada
    clicar em mais de 2 cartas antes delas retornarem a posição original

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
     this.classList.add('flip');
     if(!hasFlippedCard) {
         hasFlippedCard = true;
@@ -22,6 +25,26 @@ function checkForMath() {
     }
 
     unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
