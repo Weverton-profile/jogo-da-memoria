276a056785281405d4e6daae02ac881cb6c49ea6
Weverton-profile <wevertonalves.profile@gmail.com>
2022-07-02 15:51:51 -0300

    implementando função de embaralhar e resetar o jogo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let cont = 0;
 
 function flipCard() {
     if(lockBoard) return;
@@ -20,6 +21,12 @@ function flipCard() {
 
 function checkForMath() {
     if(firstCard.dataset.card === secondCard.dataset.card) {
+        cont += 1;
+        if(cont >= 6) {
+           setTimeout(() => {
+                window.location.reload()
+           }, 1500);
+        }
         disableCards();
         return;
     }
@@ -39,13 +46,20 @@ function unflipCards() {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         resetBoard();
-    }, 1500);
+    }, 1000);
 }
 
 function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
+
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition
+    });
+})();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
