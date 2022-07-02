2d6d8bdeb786db994c58ba98f2d58c918535e404
Weverton-profile <wevertonalves.profile@gmail.com>
2022-07-02 14:45:40 -0300

    animação de clique nas cartas

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll('.card');
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+})
